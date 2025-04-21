ac01241c05029cabb5c9b3a7eec6a2e83b825e3a
Janice Nguyen <haidn@uci.edu>
2025-04-21 01:26:08 -0700

    implement options scene

---
+++ Assets/Scripts/OptionsScript.cs
@@ -0,0 +1,96 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.Rendering;
+
+public class OptionsScript : MonoBehaviour
+{
+    public static OptionsScript instance;
+    public AudioMixer mainAudioMixer;
+
+    public Slider sfxSlider;
+    public Slider musicSlider;
+
+    private bool isInitialized = false;
+
+    public CanvasGroup smSound, mdSound, lgSound, mtSound;
+    public CanvasGroup music, mtMusic;
+
+    private void Start()
+    {
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVol", 0f);
+        float musicVolume = PlayerPrefs.GetFloat("MusicVol", 0f);
+        sfxSlider.value = sfxVolume;
+        musicSlider.value = musicVolume;
+
+
+        if (!isInitialized)
+        {
+            isInitialized = true;
+        }
+        else
+        {
+            SetSFX(sfxVolume);
+            SetMusic(musicVolume);
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetSFX(float volume)
+    {
+        Debug.Log("Volume set to: " + volume);
+        mainAudioMixer.SetFloat("SFXVol", volume);
+        if (volume == -80f)
+        {
+            mtSound.alpha = 1f;
+            smSound.alpha = mdSound.alpha = lgSound.alpha = 0f;
+        }
+        else if (volume > -80f && volume < -50f)
+        {
+            smSound.alpha = 1f;
+            mdSound.alpha = lgSound.alpha = mtSound.alpha = 0f;
+        }
+        else if (volume > -50f && volume < -20f)
+        {
+            smSound.alpha = mdSound.alpha = 1f;
+            lgSound.alpha = mtSound.alpha = 0f;
+        }
+        else
+        {
+            mtSound.alpha = 0f;
+            smSound.alpha = mdSound.alpha = lgSound.alpha = 1f;
+        }
+        PlayerPrefs.SetFloat("SFXVol", volume);
+        PlayerPrefs.Save();
+    }
+
+    public void SetMusic(float volume)
+    {
+        Debug.Log("Music set to: " + volume);
+        mainAudioMixer.SetFloat("MusicVol", volume);
+        if (volume == -80f)
+        {
+            mtMusic.alpha = 1f;
+            music.alpha = 0f;
+        }
+        else
+        {
+            music.alpha = 1f;
+            mtMusic.alpha = 0f;
+        }
+        PlayerPrefs.SetFloat("MusicVol", volume);
+        PlayerPrefs.Save();
+    }
+}
